d7e53da1fd4835f5fbead8dbbcffc4702b53a046
ChoiLeo99 <choileo0910@gmail.com>
2026-01-20 22:37:39 +0900

    chore(ui): set up shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Button>버튼!</Button>
+    </div>
+  );
 }
 
 export default App;
