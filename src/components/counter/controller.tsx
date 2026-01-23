07b2218ba2d74a4cf337fb4558385d105df97b0b
ChoiLeo99 <choileo0910@gmail.com>
2026-01-23 22:07:01 +0900

    feat(store): add zustand counter store and component

---
+++ src/components/counter/controller.tsx
@@ -0,0 +1,20 @@
+import {
+  useCountStore,
+  useDecreaseCount,
+  useIncreaseCount,
+} from "@/store/count";
+import { Button } from "../ui/button";
+
+export default function Controller() {
+  const increase = useIncreaseCount();
+  const decrease = useDecreaseCount();
+
+  return (
+    <div>
+      <div>
+        <Button onClick={increase}>+</Button>
+        <Button onClick={decrease}>-</Button>
+      </div>
+    </div>
+  );
+}
