1b7073c0f6ed757734cecb9f119e0e03c5a29f54
ChoiLeo99 <choileo0910@gmail.com>
2026-02-10 23:04:09 +0900

    feat(todo) add update mutation with react-query

---
+++ src/api/create-todo.ts
@@ -0,0 +1,14 @@
+import { API_URL } from "@/lib/constants";
+
+export async function createTodo(content: string) {
+  const response = await fetch(`${API_URL}/todos`, {
+    method: "POST",
+    body: JSON.stringify({
+      content,
+      isDone: false,
+    }),
+  });
+  if (!response.ok) throw new Error("Create Todo Failed");
+
+  const data = await response.json();
+}
